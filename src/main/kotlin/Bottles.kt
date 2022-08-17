7c3a9ff23d47e04bd432c6c01540ce84e4c7f631
Andrew Schrader <andrew.schrader@nielseniq.com>
2022-08-17 13:58:33 -0500

    Compounding Conditional Sins

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -27,6 +27,18 @@ class Bottles {
                         "Take one down and pass it around, " +
                         "1 bottle of beer on the wall."
             }
+            6 -> {
+                "1 six-pack of beer on the wall, " +
+                        "1 six-pack of beer.\n" +
+                        "Take one down and pass it around, " +
+                        "5 bottles of beer on the wall."
+            }
+            7 -> {
+                "7 bottles of beer on the wall, " +
+                        "7 bottles of beer.\n" +
+                        "Take one down and pass it around, " +
+                        "1 six-pack of beer on the wall."
+            }
             else -> {
                 "$number bottles of beer on the wall, " +
                         "$number bottles of beer.\n" +
--- src/test/kotlin/BottlesTest.kt
+++ src/test/kotlin/BottlesTest.kt
@@ -98,8 +98,8 @@ internal class BottlesTest {
                     "10 bottles of beer on the wall, 10 bottles of beer.\nTake one down and pass it around, 9 bottles of beer on the wall.\n" +
                     "9 bottles of beer on the wall, 9 bottles of beer.\nTake one down and pass it around, 8 bottles of beer on the wall.\n" +
                     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n" +
-                    "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n" +
-                    "6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n" +
+                    "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 1 six-pack of beer on the wall.\n" +
+                    "1 six-pack of beer on the wall, 1 six-pack of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n" +
                     "5 bottles of beer on the wall, 5 bottles of beer.\nTake one down and pass it around, 4 bottles of beer on the wall.\n" +
                     "4 bottles of beer on the wall, 4 bottles of beer.\nTake one down and pass it around, 3 bottles of beer on the wall.\n" +
                     "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n" +
