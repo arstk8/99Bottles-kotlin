249c6baddbe6feafdea4f4c323386e0edb2ff916
Andrew Schrader <andrew.schrader@nielseniq.com>
2022-08-17 14:13:34 -0500

    Refactor "2" case out by adding container method

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -21,30 +21,20 @@ class Bottles {
                         "Take it down and pass it around, " +
                         "no more bottles of beer on the wall."
             }
-            2 -> {
-                "2 bottles of beer on the wall, " +
-                        "2 bottles of beer.\n" +
-                        "Take one down and pass it around, " +
-                        "1 bottle of beer on the wall."
-            }
-            6 -> {
-                "1 six-pack of beer on the wall, " +
-                        "1 six-pack of beer.\n" +
-                        "Take one down and pass it around, " +
-                        "5 bottles of beer on the wall."
-            }
-            7 -> {
-                "7 bottles of beer on the wall, " +
-                        "7 bottles of beer.\n" +
-                        "Take one down and pass it around, " +
-                        "1 six-pack of beer on the wall."
-            }
             else -> {
                 "$number bottles of beer on the wall, " +
                         "$number bottles of beer.\n" +
                         "Take one down and pass it around, " +
-                        "${number - 1} bottles of beer on the wall."
+                        "${number - 1} ${container(number - 1)} of beer on the wall."
             }
+        }
+    }
+
+    private fun container(number: Int): String {
+        return if (number == 1) {
+            "bottle"
+        } else {
+            "bottles"
         }
     }
 }
