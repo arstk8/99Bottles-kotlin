bb55e8e09590c87115675bf928a9ed591aa0854c
Andrew Schrader <andrew.schrader@nielseniq.com>
2022-08-17 13:37:44 -0500

    Speculatively General

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -8,10 +8,47 @@ class Bottles {
     }
 
     private fun verse(n: Int): String {
-        return "${if (n == 0) "No more" else n} bottle${if (n == 1) "" else "s"}" +
-                " of beer on the wall, " +
-                "${if (n == 0) "no more" else n} bottle${if (n == 1) "" else "s"} of beer.\n" +
-                (if (n > 0) "Take ${if (n > 1) "one" else "it"} down and pass it around, " else "Go to the store and buy some more, ") +
-                        "${if (n - 1 < 0) 99 else if (n - 1 == 0) "no more" else n - 1} bottle${if (n - 1 == 1) "" else "s"} of beer on the wall."
+        return verseFor(n).text()
+    }
+
+    private fun verseFor(n: Int): Verse {
+        return when (n) {
+            0 -> Verse(n, this::noMore)
+            1 -> Verse(n, this::lastOne)
+            2 -> Verse(n, this::penultimate)
+            else -> Verse(n, this::default)
+        }
+    }
+
+    private fun noMore(verse: Verse): String {
+        return "No more bottles of beer on the wall, " +
+                "no more bottles of beer.\n" +
+                "Go to the store and buy some more, " +
+                "99 bottles of beer on the wall."
+    }
+
+    private fun lastOne(verse: Verse): String {
+        return "1 bottle of beer on the wall, " +
+                "1 bottle of beer.\n" +
+                "Take it down and pass it around, " +
+                "no more bottles of beer on the wall."
+    }
+
+    private fun penultimate(verse: Verse): String {
+        return "2 bottles of beer on the wall, " +
+                "2 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "1 bottle of beer on the wall."
+    }
+
+    private fun default(verse: Verse): String {
+        return "${verse.number} bottles of beer on the wall, " + "${verse.number} bottles of beer.\n" +
+                "Take one down and pass it around, " + "${verse.number - 1} bottles of beer on the wall.";
+    }
+}
+
+class Verse(val number: Int, private val lyrics: (verse: Verse) -> String) {
+    fun text(): String {
+        return lyrics(this)
     }
 }
